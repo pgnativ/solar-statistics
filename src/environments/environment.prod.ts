7edae4682d97782854ea6b6d7b7710e4a2e843b0
pavlo.gnativ <pgnativ@gmail.com>
2018-06-19 18:55:52 +0300

    firebase config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBwMkf0EenUvNvRPPBb46EqyscnVOZpyh4",
+    authDomain: "solar-statistics-test.firebaseapp.com",
+    databaseURL: "https://solar-statistics-test.firebaseio.com",
+    projectId: "solar-statistics-test",
+    storageBucket: "solar-statistics-test.appspot.com",
+    messagingSenderId: "239587634838"
+  }
+};
