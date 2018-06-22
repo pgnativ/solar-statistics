3d8894a9e4e0c1bdbfaa3946c085c387eb6672c8
pgnativ <pgnativ@gmail.com>
2018-06-22 03:43:43 +0300

    temporary commit: research modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+//import {  } from '@angular/cdk';
+import { MatDialogModule } from '@angular/material';
+//import {config} from 'rxjs/index';
+
 import { StatsItemComponent } from './stats/stats-item/stats-item.component';
+import { AddStatsItemComponent } from './stats/add-stats-item/add-stats-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StatsItemComponent
+    StatsItemComponent,
+    AddStatsItemComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     AngularFireDatabaseModule,
+    MatDialogModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
--- src/app/stats/stats-item/stats-item.component.ts
+++ src/app/stats/stats-item/stats-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+//import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-stats-item',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
   }
 
+  private addItem() {
+    const dialogRef = this.dialog.open(StatsItemAddComponent);
+  }
+
+  /*private addItem() {
+    let dialogRef = this.dialog.open(StatsItemAddComponent, {
+      height: '400px',
+      width: '600px',
+    });
+  }*/
+
 }
+
+@Component({
+  templateUrl: 'stats-item-add.component.html',
+})
+export class StatsItemAddComponent {
+
+  /*constructor(
+    public dialogRef: MatDialogRef<StatsItemAddComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }*/
+
+}
+
+/*@Component({
+  selector: 'stats-item-add',
+  templateUrl: 'stats-item-add.component.html',
+})
+export class StatsItemAddComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<StatsItemAddComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}*/
