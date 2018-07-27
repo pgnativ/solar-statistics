54d37b8424bea4ccef8227a1595272947a48a2d0
pgnativ <pgnativ@gmail.com>
2018-07-27 11:10:29 +0300

    routing home and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { AppRouting } from './app.routing.module';
+import { ListPageComponent } from './stats/list-page/list-page.component';
+import { HomePageComponent } from './stats/home-page/home-page.component';
+import { HeaderComponent } from './stats/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPageComponent,
+    HomePageComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
+    AppRouting,
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
